f81c0cc8a910e1a7ccdea1010b8806414495ddba
Amrit Raj <Amrit Raj@DESKTOP-EFURESQ>
2023-05-08 17:04:52 +0530

    First commit
    Uploading all the files

---
+++ src/main/java/com/admin/servlet/AddSpecialist.java
@@ -0,0 +1,42 @@
+package com.admin.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dao.SpecialistDao;
+import com.db.DBConnect;
+
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/addSpecialist")
+public class AddSpecialist extends HttpServlet{
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String specName = req.getParameter("specName");
+		
+		SpecialistDao dao = new SpecialistDao(DBConnect.getConn());
+		boolean f = dao.addSpecialist(specName);
+		
+		HttpSession session = req.getSession();
+		
+		if(f)
+		{
+			session.setAttribute("sucMsg", "Specialist Added");
+			resp.sendRedirect("admin/index.jsp");
+		}
+		else {
+			session.setAttribute("errorMsg", "Something Wrong on server");
+			resp.sendRedirect("admin_login.jsp");
+		}
+	}
+	
+	
+
+}
